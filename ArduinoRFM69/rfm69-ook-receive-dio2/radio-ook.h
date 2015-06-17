b0ab519a4a91db11ff78b2e2c5c9aa8fef129fab
Seven Watt <info@sevenwatt.com>
2015-06-17 22:51:06 +0200

    Add rfm69-ook DIO2/DATA example

---
+++ ArduinoRFM69/rfm69-ook-receive-dio2/radio-ook.h
@@ -0,0 +1,276 @@
+// OOK/RSSI RF69 driver
+
+class RF69A {
+public:
+    void init (uint8_t id, uint8_t group, int freq);
+    uint8_t readRSSI();
+    int16_t readAFC();
+    int16_t readFEI(bool start);
+    int readStatus();
+    void setThd(uint8_t thd);
+    void setBW(uint8_t bw);
+    void setFrequency(uint32_t frq);
+    void setBitrate(uint32_t br);
+
+    //test functions
+    void DataModule(uint8_t module);
+    void OOKthdMode(uint8_t thdmode);
+    void readAllRegs();
+
+    uint8_t myGroup;
+
+protected:
+    enum {
+        REG_FIFO          = 0x00,
+        REG_OPMODE        = 0x01,
+        REG_DATAMOD       = 0x02,
+        REG_BRMSB         = 0x03,
+        REG_FRFMSB        = 0x07,
+        REG_PALEVEL       = 0x11,
+        REG_OOKPEAK       = 0x1B,
+        REG_OOKFIX        = 0x1D,
+        REG_AFCFEI        = 0x1E,
+        REG_AFCMSB        = 0x1F,
+        REG_AFCLSB        = 0x20,
+        REG_FEIMSB        = 0x21,
+        REG_FEILSB        = 0x22,
+        REG_RSSICONFIG    = 0x23,
+        REG_RSSIVALUE     = 0x24,
+        REG_IRQFLAGS1     = 0x27,
+        REG_IRQFLAGS2     = 0x28,
+        REG_SYNCVALUE1    = 0x2F,
+        REG_SYNCVALUE2    = 0x30,
+        REG_NODEADDR      = 0x39,
+        REG_BCASTADDR     = 0x3A,
+        REG_PKTCONFIG2    = 0x3D,
+        REG_AESKEYMSB     = 0x3E,
+        
+        REG_TEMP1         = 0x4E,
+        REG_TEMP2         = 0x4F,
+
+        MODE_SLEEP        = 0<<2,
+        MODE_TRANSMIT     = 3<<2,
+        MODE_RECEIVE      = 4<<2,
+
+        IRQ1_MODEREADY    = 1<<7,
+        IRQ1_RXREADY      = 1<<6,
+
+        IRQ2_FIFOFULL     = 1<<7,
+        IRQ2_FIFONOTEMPTY = 1<<6,
+        IRQ2_FIFOOVERRUN  = 1<<4,
+        IRQ2_PACKETSENT   = 1<<3,
+        IRQ2_PAYLOADREADY = 1<<2,
+    };
+    
+    void configure (const uint8_t* p);
+    void writeReg (uint8_t addr, uint8_t value);
+    uint8_t readReg (uint8_t addr);
+    void setMode (uint8_t newMode);
+
+    volatile uint8_t mode;
+};
+
+// driver implementation
+
+static const uint8_t configRegsOOK [] = {
+    0x01, 0x04, // OpMode = standby
+    0x02, 0x68, // DataModul = conti mode, ook, no shaping
+    //0x02, 0x60, // DataModul = conti mode, fsk, no shaping
+    //0x02, 0x00, // DataModul = packet mode, fsk, no shaping
+    //0x03, 0x7D, // BitRateMsb, 1000bps
+    //0x04, 0x00, // BitRateLsb, divider = 32 MHz / 650
+    //0x03, 0x00, // BitRateMsb, data rate = 300 kbps max FSK
+    //0x04, 0x6B, // BitRateLsb, divider = 32 MHz / 650
+    0x03, 0x03, // BitRateMsb, data rate = 32768bps maxOOK
+    0x04, 0xD1, // BitRateLsb, divider = 32 MHz / 650
+    //0x05, 0x01, // FdevMsb = 25 KHz
+    //0x06, 0x99, // FdevLsb = 25 KHz
+    //0x07, 0xD9, // FrfMsb, freq = 868.250 MHz
+    //0x08, 0x13, // FrfMib, divider = 14221312
+    //0x09, 0x00, // FrfLsb, step = 61.03515625
+    0x0B, 0x20, // AfcCtrl, afclowbetaon
+    0x18, 0x81, // LNA fixed highest gain, Z=200ohm
+    0x19, 0x40, // RxBw DCC=4%, Man=00b Exp = 0 =>BWOOK=250.0kHz
+    0x1B, 0x43, // OOK peak, 0.5dB once/8 chips (slowest)
+    0x1C, 0x80, // OOK avg thresh /4pi
+    0x1D, 0x38, // OOK fixed thresh
+    0x1E, 0x00, // No auto AFC
+    0x25, 0x80, // Dio 0: RSSI 1: Dclk 2: Data 3: RSSI
+    0x26, 0x37, // Dio 5: ModeReady, CLKOUT = OFF
+    0x29, 0xFF, // RssiThresh: lowest possible threshold to start receive
+    0x2E, 0x00, // SyncConfig = sync off
+    0x3C, 0x8F, // FifoTresh, not empty, level 15
+    0x3D, 0x10, // PacketConfig2, interpkt = 1, autorxrestart off
+    //0x58, 0x2D, // Sensitivity boost (TestLNA)
+    0x6F, 0x20, // TestDagc ...
+    0
+};
+
+void RF69A::init (uint8_t id, uint8_t group, int freq) {
+    //RF69<SPI>::init(id, group, freq);
+    this->configure(configRegsOOK);
+    //clear AFC. Essential for wideband OOK signals. No other way to reset from SW.
+    this->writeReg(REG_AFCFEI, (1<<1));
+    //this->writeReg(REG_AFCFEI, this->readReg(REG_AFCFEI) | (1 << 1)); //does not work
+    myGroup = group;
+    this->setMode(MODE_RECEIVE);
+}
+
+void RF69A::writeReg (uint8_t addr, uint8_t value) {
+    RF69::control(addr | 0x80, value);
+}
+
+uint8_t RF69A::readReg (uint8_t addr) {
+    return RF69::control(addr, 0);
+}
+
+void RF69A::configure (const uint8_t* p) {
+    while (true) {
+        uint8_t cmd = p[0];
+        if (cmd == 0)
+            break;
+        writeReg(cmd, p[1]);
+        p += 2;
+    }
+}
+
+void RF69A::setMode (uint8_t newMode) {
+  mode = newMode;
+  writeReg(REG_OPMODE, (readReg(REG_OPMODE) & 0xE3) | newMode);
+  while ((readReg(REG_IRQFLAGS1) & IRQ1_MODEREADY) == 0)
+    ;
+}
+
+
+uint8_t RF69A::readRSSI() {
+    uint8_t rssi = 0;
+    //  if (true)
+    //  {
+    //    //RSSI trigger not needed if DAGC is in continuous mode
+    //    writeReg(REG_RSSICONFIG, RF_RSSI_START);
+    //    while ((readReg(REG_RSSICONFIG) & RF_RSSI_DONE) == 0x00); // Wait for RSSI_Ready
+    //  }
+    rssi = this->readReg(REG_RSSIVALUE);
+    //rssi >>= 1;
+    return rssi;
+}
+
+int16_t RF69A::readFEI(bool start) {
+    int16_t fei = 0;
+      if (start)
+      {
+        this->writeReg(REG_AFCFEI, 1 << 5);
+        while ((this->readReg(REG_AFCFEI) & (1 << 6)) == 0x00); // Wait for RSSI_Ready
+      }
+    fei = this->readReg(REG_FEILSB);
+    fei |= (this->readReg(REG_FEIMSB) << 8);
+    return fei;
+}
+
+int16_t RF69A::readAFC() {
+    int16_t afc = 0;
+    //  if (true)
+    //  {
+    //    //RSSI trigger not needed if DAGC is in continuous mode
+    //    writeReg(REG_RSSICONFIG, RF_RSSI_START);
+    //    while ((readReg(REG_RSSICONFIG) & RF_RSSI_DONE) == 0x00); // Wait for RSSI_Ready
+    //  }
+    afc = this->readReg(REG_AFCLSB);
+    afc |= (this->readReg(REG_AFCMSB) << 8);
+    //reset AFC
+//    if (true)
+//    {
+//        this->writeReg(REG_AFCFEI, (1<<1));
+//        //this->writeReg(REG_AFCFEI, this->readReg(REG_AFCFEI) | (1 << 1)); //does not work
+//        //while ((this->readReg(REG_AFCFEI) & (1 << 6)) == 0x00); // Wait for RSSI_Ready
+//    }
+
+    return afc;
+}
+
+int RF69A::readStatus() {
+    uint8_t f1;
+    uint8_t f2;
+    switch (this->mode) {
+    case MODE_RECEIVE:
+    case MODE_TRANSMIT:
+        f1 = readReg(REG_IRQFLAGS1);
+        f2 = readReg(REG_IRQFLAGS2);
+        if (f1 & 0x08) {
+            //Serial.print( BYTETOBINARYPATTERN, BYTETOBINARY(f1));
+            //Serial.print( "  ");
+            //Serial.print( BYTETOBINARYPATTERN, BYTETOBINARY(f2));
+            //Serial.print( "  ");
+            //fixthd += 1;
+            //writeReg(0x1D, fixthd);
+            //Serial.print(fixthd);
+            //Serial.print(" ");
+            Serial.println(readRSSI());
+        }
+    default:
+        break;
+    }
+    return 0;
+}
+
+void RF69A::setThd (uint8_t thd) {
+    this->writeReg(REG_OOKFIX, thd);
+}
+
+void RF69A::setBW (uint8_t bw) {
+    this->writeReg(0x19, 0x40 | bw);
+}
+
+void RF69A::setFrequency (uint32_t frq) {
+    this->writeReg(REG_AFCFEI, (1<<1));
+    //RF69<SPI>::setFrequency(frq);
+
+    // accept any frequency scale as input, including KHz and MHz
+    while (frq < 100000000)
+        frq *= 10;
+    uint64_t frf = ((uint64_t)frq << 8) / (32000000L >> 11);
+    this->writeReg(REG_FRFMSB, frf >> 16);
+    this->writeReg(REG_FRFMSB+1, frf >> 8);
+    this->writeReg(REG_FRFMSB+2, frf);
+}
+
+void RF69A::setBitrate (uint32_t br) {
+    if (br < 489) {
+        br = 0xFFFF;
+    } else {
+        br = 32000000L / br;
+    }
+    this->writeReg(REG_BRMSB, br >> 8);
+    this->writeReg(REG_BRMSB+1, br);
+}
+
+void RF69A::DataModule(uint8_t module) {
+    this->setMode(MODE_SLEEP);
+    this->writeReg(REG_DATAMOD, module);
+    delayMicroseconds(400);
+    this->setMode(MODE_RECEIVE);
+}
+
+void RF69A::OOKthdMode(uint8_t thdmode) {
+    this->writeReg(REG_OOKPEAK, thdmode);
+}
+
+
+////for debugging
+//void RF69A::readAllRegs() {
+//    uint8_t regVal;
+//
+//    Serial.print( "     0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  D"
+//    "\r\n00: 00");
+//    for (uint8_t regAddr = 1; regAddr <= 0x4F; regAddr++) {
+//        this->spi.enable();
+//        this->spi.transfer(regAddr & 0x7f);   // send address + r/w bit
+//        regVal = this->spi.transfer(0);
+//        this->spi.disable();
+//
+//        if (regAddr % 16 == 0)
+//        Serial.print( "\r\n%02X:", regAddr);
+//        Serial.print( " %02x", regVal);
+//    }
+//    Serial.print( "\r\n");
+//}
---
+++ ArduinoRFM69/rfm69-ook-receive-dio2/rfm69-ook-receive-dio2.ino
@@ -0,0 +1,410 @@
+#define RF69_COMPAT 1 // define this to use the RF69 driver i.s.o. RF12
+#include <JeeLib.h>
+//#include <Time.h>
+#include "radio-ook.h"
+#include "decodeOOK.h"
+//#include "decodeOOK_TEST.h"
+
+// Config items ------------------------------------------------------------
+#define RF69_RX_DATA 3	//D3 = bit 3 in PORTD - JeeLink V3C + solder-bridge!
+#define RF69_RX_EXTINT 1
+#define RF69_RX_PIN PIND
+#define RF69_RX_DDR DDRD
+
+#define FREQ_BAND 868 //868 or 433
+#define SERIAL_BAUD 57600
+#define STATLOG 1 //0=no statistics logging 1=statistics logging
+
+#if FREQ_BAND == 433
+#define RF12_BAND RF12_433MHZ
+uint32_t frqkHz = 433920;
+#else
+#define RF12_BAND RF12_868MHZ
+uint32_t frqkHz = 868400;
+#endif
+
+uint8_t tsample = 25; //25 us samples
+//static volatile uint8_t fixthd = 0x10;
+uint8_t fixthd = 0x10;
+uint32_t bitrate = 32768;
+uint8_t bw = 16; //0=250kHz, 8=200kHz, 16=167kHz, 1=125kHz, 9=100kHz, 17=83kHz 2=63kHz, 10=50kHz
+
+#if FREQ_BAND == 433
+//433MHz
+#include "decoders433.h"
+WS249 ws249;
+Philips phi;
+OregonDecoderV1 orscV1;
+//OregonDecoderV2 orscV2;
+//OregonDecoderV3 orscV3;
+//CrestaDecoder cres;
+KakuDecoder kaku;
+//KakuADecoder kakuA;
+//XrfDecoder xrf;
+//HezDecoder hez;
+#else
+//868MHz
+#include "decoders868.h"
+//VisonicDecoder viso;
+EMxDecoder emx;
+//KSxDecoder ksx;
+FSxDecoder fsx;
+//FSxDecoderA fsxa;
+//
+#endif
+
+// End config items --------------------------------------------------------
+
+//global instance of the OOK RF69 class
+RF69A rf;
+
+void printDigits(int val) {
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if (val < 10)
+    Serial.print('0');
+  Serial.print(val);
+}
+
+void printHex(int val) {
+  if (val < 16)
+    Serial.print('0');
+  Serial.print(val, HEX);
+}
+
+////small circular buffer for ON rssi signals
+//#define RSSI_BUF_EXP 3 //keep it powers of 2
+//#define RSSI_BUF_SIZE  (1<<RSSI_BUF_EXP)
+//uint8_t rssi_buf[RSSI_BUF_SIZE];
+//uint8_t rssi_buf_i = 0;
+//
+//void printRSSI() {
+//  uint16_t avgonrssi = 0;
+//  uint16_t avgoffrssi = 0;
+//  for (uint8_t i = 0; i < RSSI_BUF_SIZE; i += 2) {
+//    avgonrssi += rssi_buf[i];
+//    avgoffrssi += rssi_buf[i + 1];
+//    //Serial.print(" (");
+//    //Serial.print(rssi_buf[i+1]);
+//    //Serial.print("/");
+//    //Serial.print(rssi_buf[i]);
+//    //Serial.print(") ");
+//  }
+//  Serial.print(" (");
+//  Serial.print(avgoffrssi >> RSSI_BUF_EXP - 1);
+//  Serial.print("/");
+//  Serial.print(avgonrssi >> RSSI_BUF_EXP - 1);
+//  Serial.print(")");
+//}
+
+void reportOOK (const char* s, class DecodeOOK& decoder) {
+  uint8_t pos;
+  const uint8_t* data = decoder.getData(pos);
+  //Serial.println("");
+  //Serial.print(hour());
+  //printDigits(minute());
+  //printDigits(second());
+  //Serial.print(" ");
+  //int8_t textbuf[25];
+  //rf12_settings_text(textbuf);
+  //Serial.print(textbuf);
+  //Serial.print(' ');
+  Serial.print(s);
+  Serial.print(' ');
+  for (uint8_t i = 0; i < pos; ++i) {
+    printHex(data[i]);
+  }
+  //printRSSI();
+  Serial.println();
+
+  decoder.resetDecoder();
+}
+
+#if FREQ_BAND == 433
+void processBit(uint16_t pulse_dur, uint8_t signal, uint8_t rssi) {
+//  if (rssi) {
+//    if (signal) {
+//      rssi_buf_i += 2;
+//      rssi_buf_i &= (RSSI_BUF_SIZE - 2);
+//      rssi_buf[rssi_buf_i] = rssi;
+//    } else {
+//      rssi_buf[rssi_buf_i + 1] = rssi;
+//    }
+//  }
+  //Serial.print( "\r\nf=%3d.%3dkHz BW-idx=%2d, OOKTHd = %d - ", frqkHz/1000, frqkHz%1000, bw, fixthd);
+  if (orscV1.nextPulse(pulse_dur, signal)) {
+    reportOOK("ORSCV1 ", orscV1);
+    //set scope trigger
+    //palWritePad(GPIOB, 4, 1);
+  }
+  if (kaku.nextPulse(pulse_dur, signal))
+    reportOOK("kaku", kaku);
+  if (ws249.nextPulse(pulse_dur, signal))
+    reportOOK("WS249", ws249);
+  if (phi.nextPulse(pulse_dur, signal)) {
+    reportOOK("phi", phi);
+    //set scope trigger
+    //palWritePad(GPIOB, 4, 1);
+  }
+}
+#else
+void processBit(uint16_t pulse_dur, uint8_t signal, uint8_t rssi) {
+//  if (rssi) {
+//    if (signal) {
+//      rssi_buf_i += 2;
+//      rssi_buf_i &= (RSSI_BUF_SIZE - 2);
+//      rssi_buf[rssi_buf_i] = rssi;
+//    } else {
+//      rssi_buf[rssi_buf_i + 1] = rssi;
+//    }
+//  }
+  //Serial.print( "\r\nf=%3d.%3dkHz BW-idx=%2d, OOKTHd = %d - ", frqkHz/1000, frqkHz%1000, bw, fixthd);
+  if (emx.nextPulse(pulse_dur, signal)) {
+    reportOOK("EMX  ", emx);
+    //set scope trigger
+    //palWritePad(GPIOB, 4, 1);
+  }
+  if (fsx.nextPulse(pulse_dur, signal)) {
+    reportOOK("FS20 ", fsx);
+    //set scope trigger
+    //palWritePad(GPIOB, 4, 1);
+  }
+  //  if (fsxa.nextPulse(pulse_dur, signal)) {
+  //    reportOOK("FS20A", fsxa);
+  //    //set scope trigger
+  //    //palWritePad(GPIOB, 4, 1);
+  //  }
+}
+#endif
+
+static void receiveOOK() {
+  //moving average buffer
+  uint8_t avg_len = 5;
+  uint32_t filter = 0;
+  uint32_t sig_mask = (1 << avg_len) - 1;
+
+  rf.OOKthdMode(0x40); //0x00=fix, 0x40=peak, 0x80=avg
+  rf.setBitrate(bitrate);
+
+  rf.setFrequency(frqkHz);
+  rf.setBW(bw);
+  rf.setThd(fixthd);
+  //rf.readAllRegs();
+  uint8_t t_step = tsample;
+  uint32_t now = micros();
+  uint32_t soon = now + t_step;
+
+  uint32_t nrssi = 0;
+  uint64_t sumrssi = 0;
+  uint64_t sumsqrssi = 0;
+  uint32_t rssivar = 0;
+
+  uint8_t rssimax = fixthd + 6;
+  uint8_t slicethd = 100;
+  uint8_t decthdcnt = 0;
+  uint8_t prev_thd = 0;
+  uint8_t max_thd = 0;
+  uint8_t prev_rssi = 0;
+  uint16_t log = 0;
+
+  uint32_t new_edge = now;
+  uint32_t last_edge = now;
+  uint8_t last_data = bitRead(RF69_RX_PIN, RF69_RX_DATA);
+
+  //uint8_t last_data = ~rf.readRSSI() > slicethd;
+
+  uint16_t flip_cnt = 0;
+
+  uint32_t ts_rssi = micros();
+  uint8_t rssi = ~rf.readRSSI();
+  uint32_t thdUpd = millis();
+  uint32_t thdUpdCnt = 0;
+
+  //Calculate RSSI during OOK pulse train.
+  uint8_t last_steady_rssi = 0;
+//  uint8_t rssi_q_off = 2; //stay >75us away from flip. 2 samples.
+//  uint8_t rssi_q_len = avg_len + rssi_q_off + 1;
+//  uint8_t rssi_q[rssi_q_len];
+//  uint8_t rssi_qi = 0;
+
+  while (true) {
+    //rssi = ~rf.readRSSI();
+    //statistics update every 1ms
+    if ((micros() - ts_rssi) > (1000)) {
+      rssi = ~rf.readRSSI();
+      sumrssi += rssi;
+      sumsqrssi += rssi * rssi;
+      nrssi++;
+      ts_rssi = micros();
+      if (rssi > rssimax) rssimax = rssi;
+    }
+
+//    //emulate PEAK-mode OOK threshold
+//    decthdcnt++;
+//    if (((decthdcnt & 3) == 0) and (slicethd > fixthd)) {
+//      //decrement slicethd every 4th poll (~280us) with 0.5dB
+//      slicethd--;
+//    }
+//    prev_thd = slicethd;
+//    if (rssi > slicethd + 12) {
+//      //deal with outlier rssi?
+//      slicethd = rssi - 12;
+//      //limit printing
+//      if (slicethd > prev_thd + 12) {
+//        //Serial.print( "PEAK-THD: set %d\r\n", slicethd);
+//      }
+//      if (rssi > 200) {
+//        slicethd = 188;
+//        Serial.print(F("PEAK-THD: outlier rssi "));
+//        Serial.println(rssi);
+//      }
+//      if (slicethd < fixthd) slicethd = fixthd;
+//    }
+//    if (slicethd > max_thd) max_thd = slicethd;
+
+    uint8_t data_in = bitRead(RF69_RX_PIN, RF69_RX_DATA);
+    //uint8_t data_in = rssi > slicethd;
+    filter = (filter << 1) | (data_in & 0x01);
+    //efficient bit count, from literature
+    uint8_t c = 0;
+    uint32_t v = filter & sig_mask;
+    for (c = 0; v; c++)
+    {
+      v &= v - 1; // clear the least significant bit set
+    }
+    uint8_t data_out = (c > (avg_len >> 1)); //bit count moving average
+    //filtered DATA to scope
+    //palWritePad(GPIOB, 5, (data_out & 0x01));
+
+//    //delay rssi to sync with moving average data
+//    //delay: half the average buffer + 50-75us further back (3 samples)
+//    uint8_t j = rssi_qi + rssi_q_len - (avg_len >> 1) - rssi_q_off;
+//    if (j >= rssi_q_len)
+//      j -= rssi_q_len;
+//    uint8_t delayed_rssi = rssi_q[j];
+//    rssi_q[rssi_qi++] = rssi;
+//    if (rssi_qi >= rssi_q_len)
+//      rssi_qi = 0;
+
+    uint32_t ts_thdUpdNow = millis();
+
+    if (data_out != last_data) {
+      new_edge = micros();
+      processBit(new_edge - last_edge, last_data, last_steady_rssi);
+      last_edge = new_edge;
+      last_data = data_out;
+      flip_cnt++;
+    } else if (micros() - last_edge > 10000) {
+      //send fake pulse to notify end of transmission to decoders
+      processBit(micros() - last_edge, last_data, 0);
+      processBit(1, !last_data, 0);
+    } /* else
+      last_steady_rssi = delayed_rssi; */
+
+    //Update minimum slice threshold (fixthd) every 10s
+    if (ts_thdUpdNow - thdUpd >= 10000) {
+      //rssivar = ((sumsqrssi - sumrssi*sumrssi/nrssi) / (nrssi-1)); //64 bits
+      rssivar = ((sumsqrssi - ((sumrssi >> 8) * ((sumrssi << 8) / nrssi))) / (nrssi - 1)); //32bits n<65000
+      uint32_t rssiavg = sumrssi / nrssi;
+      //determine stddev (no sqrt avaialble?)
+      uint8_t stddev = 1;
+      while (stddev < 10) {
+        //safe until sqr(15)
+        if (stddev * stddev >= rssivar) break;
+        stddev++;
+      }
+      //printf("n=%d, sum=%d, sumsqr=%d\n", nrssi, sumrssi, sumsqrssi);
+      //adapt rssi thd to noise level if variance is low
+      if (rssivar < 36) {
+        uint8_t delta_thd = 3 * stddev;
+        if (delta_thd < 6)  delta_thd = 6;
+        if (fixthd != rssiavg + delta_thd) {
+          fixthd = rssiavg + delta_thd;
+          //if (fixthd < 70) fixthd = 70;
+          rf.setThd(fixthd);
+          //printf("THD:%3d\r\n", fixthd);
+        } else {
+          //printf("THD:keep\r\n");
+        }
+      } else {
+        //printf("THD:keep\r\n");
+      }
+      if (STATLOG) {
+        Serial.print("RSSI: ");
+        Serial.print(rssiavg);
+        Serial.print("(v");
+        Serial.print(rssivar);
+        Serial.print("-s");
+        Serial.print(stddev);
+        Serial.print("-m");
+        Serial.print(rssimax);
+        //Serial.print(F(") THD:fix/peak/maxp:"));
+        Serial.print(F(") THD:"));
+        Serial.println(fixthd);
+        //Serial.print("/");
+        //Serial.print(slicethd);
+        //Serial.print("/");
+        //Serial.println(max_thd);
+
+        Serial.print(thdUpdCnt);
+        Serial.print(F(" polls took "));
+        Serial.print(ts_thdUpdNow - thdUpd);
+        Serial.print("ms = ");
+        Serial.print((1000 * (ts_thdUpdNow - thdUpd)) / thdUpdCnt);
+        Serial.print("us - flips = ");
+        Serial.println(flip_cnt);
+      }
+      nrssi = sumrssi = sumsqrssi = rssimax = max_thd = 0;
+      thdUpd = ts_thdUpdNow;
+      thdUpdCnt = 0;
+
+      flip_cnt = 0;
+
+      //clear scope trigger
+      //palWritePad(GPIOB, 4, 0);
+    }
+    thdUpdCnt++;
+
+    if (rssi > 120) {
+      //set scope trigger
+      //palWritePad(GPIOB, 4, 1);
+      //log = 1;
+    }
+    if (log > 0) {
+      Serial.print(rssi);
+      Serial.print(",");
+      log++;
+      if (log > 2000) log = 0;
+    }
+
+    //sleep at resolutions higher then system tick
+    int32_t delay_us = soon - micros();
+    if (delay_us > 0)
+      delayMicroseconds(delay_us);
+    soon = micros() + t_step;
+  }
+  return;
+}
+
+void setup() {
+  Serial.begin(SERIAL_BAUD);
+  Serial.print(F("\r\n[OOK-RX-dio2-poll]\r\n"));
+  //D3 (INT1) as input
+  pinMode(3, INPUT); //D3 is input
+  digitalWrite(3, HIGH); //pull up D3
+
+  //same as above?
+  RF69_RX_DDR &= ~_BV(RF69_RX_DATA);
+  detachInterrupt(RF69_RX_EXTINT);
+  
+  rf12_initialize(11, RF12_BAND, 42, 1600);// calls rf69_initialize()
+  //setup for OOK
+  rf.init(11, 42, frqkHz);
+  
+}
+
+void loop() {
+  while (true) {
+    receiveOOK();
+  }
+}
